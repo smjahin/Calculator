50a797d4509040d5a9fbe57e925b1c00c302d860
smjahin <48990475+smjahin@users.noreply.github.com>
2022-02-10 12:12:47 +0600

    Update s.js

--- s.js
+++ s.js
@@ -1,5 +1,5 @@
-var last = "";
-var next = ""; 
+var lastOperator = "";
+var nextOperator = ""; 
 var result = 0; 
 var reset = false;
 var lastInput = ""; 
@@ -18,63 +18,74 @@ function type(n){
         lastInput = $("#temp").text();
         $("#temp").text("");
         reset = false;
-        last = next;
-        next = "";
+        lastOperator = nextOperator;
+        nextOperator = "";
     }
     if($("#temp").text().length < 10){
         $("#temp").text($("#temp").text()+n);
     }
 }
 
-function nextOperation(op){
+function nextOperatorOperation(op){
     
     if (op == "sqroot"){
         $("#temp").text(sqroot($("#temp").text()));
-        next = "";
+        nextOperator = "";
         return;
     }
 
     if (op == "square"){
         $("#temp").text(square($("#temp").text()));
-        next = "";
+        nextOperator = "";
         return;
     }
 
     if (op == "inverse"){
         $("#temp").text(inverse($("#temp").text()));
-        next = "";
+        nextOperator = "";
         return;
     }
 
     if (op == "percent"){
         $("#temp").text(percent($("#temp").text()));
-        next = "";
+        nextOperator = "";
         return;
     }
 
-    if(next==0 && last.length!=0){
+    if(nextOperator==0 && lastOperator.length!=0){
         solve();
     }
 
-    next = op;
+    nextOperator = op;
     reset = true;
 }
 
 
 function sqroot(a) {
-    return Math.sqrt(Number(a)).toFixed(8);
+    
+    if(a % 1 != 0){
+        return Math.sqrt(Number(a)).toFixed(5);
+    }
+    else{
+        return Math.sqrt(Number(a), 2);
+    }
 }
 
 function square(a) {
     if(Math.pow(Number(a), 2).toFixed(8)>999999999){
         return Math.pow(Number(a), 2).toExponential(3);
     }
+    else if(a % 1 != 0){
+        return Math.pow(Number(a), 2).toFixed(5);
+    }
+    else{
+        return Math.pow(Number(a), 2);
+    }
 
-    return Math.pow(Number(a), 2).toFixed(8);
 }
 
 function inverse(a) {
-    return 1 / Number(a).toFixed(8);
+    return 1 / Number(a).toFixed(5);
 }
 
 function percent(a){
@@ -89,14 +100,23 @@ function sum(a,b){
 }
 
 function subs(a,b){
+
     if((Number(b)-Number(a)) > 999999999){
+
         return (Number(b)-Number(a)).toExponential(3);
-        }
+    }
     return (Number(b)-Number(a));
 }
 function multiply(a,b){
+
     if((Number(b)*Number(a)) > 999999999){
-    return (Number(b)*Number(a)).toExponential(3);
+
+        return (Number(b)*Number(a)).toExponential(3);
+    }
+    else if(a % 1 != 0){
+        
+        return (Number(b)*Number(a)).toFixed(5);
+    
     }
     else{
         return (Number(b)*Number(a));
@@ -114,37 +134,37 @@ function solve(){
     a = $("#temp").text();
     b = lastInput;
 
-    if(last=="sum"){
+    if(lastOperator=="sum"){
         result = sum(a,b);
     }
-    if(last=="subs"){
+    if(lastOperator=="subs"){
         result = subs(a,b);
     }
-    if(last=="multiply"){
+    if(lastOperator=="multiply"){
         result = multiply(a,b);
     }
-    if(last=="divide"){
+    if(lastOperator=="divide"){
         result = divide(a,b);
     }
-    if(last == "sqroot"){
+    if(lastOperator == "sqroot"){
         sqroot();
-        next = "";
+        nextOperator = "";
         return;
     }
-    if( last == "square"){
+    if( lastOperator == "square"){
         square(a);
-        next = "";
+        nextOperator = "";
         return;
     }
-    if( last == "inverse"){
+    if( lastOperator == "inverse"){
         inverse(a);
-        next = "";
+        nextOperator = "";
         return;
     }
 
     lastInput = $("#temp").text();
     $("#temp").text(result);
-    next = "";
+    nextOperator = "";
     reset = true;
 }
 
@@ -157,8 +177,8 @@ function clearScr() {
 // Reset everything
 function resetInput() {
     $("#temp").text("0");
-    next = "";
-    last = "";
+    nextOperator = "";
+    lastOperator = "";
     lastInput = "";
 }
 
@@ -182,7 +202,7 @@ function plusminus() {
 //input
 $(".number").click(function () {
     type($(this).text());
-    next = "";
+    nextOperator = "";
 });
 
 //operator
@@ -193,5 +213,5 @@ $(".operator").click(function () {
         solve();
         return;
     }
-    nextOperation(op);
+    nextOperatorOperation(op);
 });
